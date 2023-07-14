bbc0e5b2ea39b82f2935f98cf496e2030e8b1a1d
Ajaybabu <balivadaajay2000@gmail.com>
2023-07-14 12:47:14 +0530

    printing a number into binary

---
+++ printinbinary.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int nums[50];
+	int n,target;
+	printf("enter n:");
+	scanf("%d",&n);
+	printf("enter array elements:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&nums);
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d",nums);
+	}
+	return 0;
+}
+
